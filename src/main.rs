9b821a38bf22235b60c0645977ae45a4d1223a74
Sang Young Noh <sangyoung123@gmail.com>
2026-03-20 08:54:56 +0000

    fix: checking itp reader

--- src/main.rs
+++ src/main.rs
@@ -27,21 +27,39 @@ use sang_md::molecule::martini; // This module contains the itp reader
 use sang_md::molecule::molecule;
 
 fn main() {
+    // ------
+
     // let's try reading in a water forcefield
-    let tip3p_forcefield_string = charmm::CharmmForceField::read_file_new(
+    let tip3p_forcefield_charmm_string = charmm::CharmmForceField::read_file_new(
         "/home/sang/Desktop/Sandbox/rust/FerrumMD/ff/tip3p_example/tip3p_sample.rtf",
     );
 
     println!(
-        "We have the following string as a water tip3p forcefield
+        "We have the following string as a water tip3p forcefield (charmm)
          \n\n
          The atom types we have is as follows: {:?}
          The residue names we have is as follows: {:?}
          The bonds we have is as follows: {:?}
          ",
-        tip3p_forcefield_string.clone().unwrap().atom_types,
-        tip3p_forcefield_string.clone().unwrap().residue_name,
-        tip3p_forcefield_string.clone().unwrap().bonds,
+        tip3p_forcefield_charmm_string.clone().unwrap().atom_types,
+        tip3p_forcefield_charmm_string.clone().unwrap().residue_name,
+        tip3p_forcefield_charmm_string.clone().unwrap().bonds,
+    );
+
+    // trying the equivalent using the itp reader
+
+    // ------
+    let tip3p_forcefield_itp_string =
+        martini::MartiniForceField::read_itp("ff/Charmm27.ff/charmm27.ff/tip3p.itp");
+
+    println!(
+        "We have the following string as a water tip3p forcefield (itp)
+         \n\n
+         The atom types we have is as follows: {:?}
+         The bonds we have is as follows: {:?}
+         ",
+        tip3p_forcefield_itp_string.clone().unwrap().atoms,
+        tip3p_forcefield_itp_string.clone().unwrap().bonds,
     );
 
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
